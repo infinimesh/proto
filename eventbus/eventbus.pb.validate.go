7d8690ed51185c2386340442f9b3597686f4af1d
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2024-02-19 14:07:47 +0300

    Fix type

--- eventbus/eventbus.pb.validate.go
+++ eventbus/eventbus.pb.validate.go
@@ -58,49 +58,32 @@ func (m *Event) validate(all bool) error {
 
 	// no validation rules for EventType
 
-	{
-		sorted_keys := make([]string, len(m.GetMeta()))
-		i := 0
-		for key := range m.GetMeta() {
-			sorted_keys[i] = key
-			i++
-		}
-		sort.Slice(sorted_keys, func(i, j int) bool { return sorted_keys[i] < sorted_keys[j] })
-		for _, key := range sorted_keys {
-			val := m.GetMeta()[key]
-			_ = val
-
-			// no validation rules for Meta[key]
-
-			if all {
-				switch v := interface{}(val).(type) {
-				case interface{ ValidateAll() error }:
-					if err := v.ValidateAll(); err != nil {
-						errors = append(errors, EventValidationError{
-							field:  fmt.Sprintf("Meta[%v]", key),
-							reason: "embedded message failed validation",
-							cause:  err,
-						})
-					}
-				case interface{ Validate() error }:
-					if err := v.Validate(); err != nil {
-						errors = append(errors, EventValidationError{
-							field:  fmt.Sprintf("Meta[%v]", key),
-							reason: "embedded message failed validation",
-							cause:  err,
-						})
-					}
-				}
-			} else if v, ok := interface{}(val).(interface{ Validate() error }); ok {
-				if err := v.Validate(); err != nil {
-					return EventValidationError{
-						field:  fmt.Sprintf("Meta[%v]", key),
-						reason: "embedded message failed validation",
-						cause:  err,
-					}
-				}
+	if all {
+		switch v := interface{}(m.GetMeta()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, EventValidationError{
+					field:  "Meta",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
 			}
-
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, EventValidationError{
+					field:  "Meta",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetMeta()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return EventValidationError{
+				field:  "Meta",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
 		}
 	}
 
